c16110bdac37a78d8a0c9b41ee481cd7fcce9920
ULHTDevTeam <38860724+ULHTDevTeam@users.noreply.github.com>
2018-05-03 11:16:55 +0100

    Add files via upload

---
+++ staffrecognition/scripts/validation.js
@@ -0,0 +1,23 @@
+﻿function maxlength(element)
+{
+    if (element.GetText().length > 500)
+    {
+        element.SetText(element.GetText.substring(0, 500));
+    }
+    else {
+        txtCounter.SetText(500 - element.GetText().length);
+    }
+}
+
+var isPostbackInitiated = false;
+function OnClick(s, e) {
+    ASPxClientEdit.ValidateEditorsInContainer(null);
+    if (ASPxClientEdit.AreEditorsValid()) {
+        e.processOnServer = !isPostbackInitiated;
+        isPostbackInitiated = true;
+        setTimeout(function () {
+            s.SetEnabled(false);
+            s.SetText("Processing...");
+        }, 100);
+    }
+}
